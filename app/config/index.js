ce68c52887230b02f30e8966ad2ee6412a526141
Rohit Patil <rohitpatil07122001@gmail.com>
2022-02-28 19:22:11 +0530

    Cleaned Up config
    Signed-off-by: Rohit Patil <rohitpatil07122001@gmail.com>

--- app/config/index.js
+++ app/config/index.js
@@ -6,9 +6,5 @@ export default {
   CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:3000',
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
-  DB_NAME: process.env.DB_NAME,
-  DB_USER: process.env.DB_USER,
-  DB_HOST: process.env.DB_HOST,
-  DB_PASS: process.env.DB_PASS,
   DATABASE_URL: process.env.DATABASE_URL,
 };
