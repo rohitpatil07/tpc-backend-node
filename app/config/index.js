126c0e238cc82b4a32c7135d178eb7be0f257823
Rohit Patil <rohitpatil07122001@gmail.com>
2022-04-01 11:36:21 +0530

    Changed cors url
    Signed-off-by: Rohit Patil <rohitpatil07122001@gmail.com>

--- app/config/index.js
+++ app/config/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  CORS_ORIGIN: process.env.CORS_ORIGIN,
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
   DATABASE_URL: process.env.DATABASE_URL,
