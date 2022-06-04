5fa69e4c0a346189528696f8cc19a00f8e4ca5bf
unknown <hridayeshpadalkar7@gmail.com>
2022-06-05 00:15:35 +0530

    package wise and company wise sorting done

--- app/controllers/eligibilityControllers.js
+++ app/controllers/eligibilityControllers.js
@@ -9,5 +9,22 @@ const getEligibleStudents = async (req, res) => {
     return res.json(error);
   }
 };
-
-export default { getEligibleStudents };
+const getSelectedStudentsCompanyWise = async (req, res) => {
+  try {
+    const students = await eligibilityServices.getSelectedStudentsCompanyWise();
+    console.log(students);
+    return res.json({ students });
+  } catch (error) {
+    return res.json(error);
+  }
+};
+const getSelectedStudentsLpaWise = async (req, res) => {
+  try {
+    const students = await eligibilityServices.getSelectedStudentsLpaWise();
+    console.log(students);
+    return res.json({ students });
+  } catch (error) {
+    return res.json(error);
+  }
+}; 
+export default { getEligibleStudents,getSelectedStudentsCompanyWise,getSelectedStudentsLpaWise };
--- app/routes/eligible.js
+++ app/routes/eligible.js
@@ -3,5 +3,7 @@ import eligibilityControllers from '../controllers/eligibilityControllers.js';
 
 const router = Router();
 router.get('/top10student', eligibilityControllers.getEligibleStudents);
+router.get('/studentsplacedcompanywise',eligibilityControllers.getSelectedStudentsCompanyWise);
+router.get('/studentsplacedlpawise',eligibilityControllers.getSelectedStudentsLpaWise);
 
 export default router;
