82608dc4008b5b13e0325778e06350b07d1ed203
Rohit Patil <rohitpatil07122001@gmail.com>
2022-06-05 10:49:42 +0530

    Checks
    Signed-off-by: Rohit Patil <rohitpatil07122001@gmail.com>

--- app/routes/eligible.js
+++ app/routes/eligible.js
@@ -3,8 +3,14 @@ import eligibilityControllers from '../controllers/eligibilityControllers.js';
 
 const router = Router();
 router.get('/top10student', eligibilityControllers.getEligibleStudents);
-router.get('/studentsplacedcompanywise',eligibilityControllers.getSelectedStudentsCompanyWise);
-router.get('/studentsplacedlpawise',eligibilityControllers.getSelectedStudentsLpaWise);
+router.get(
+  '/studentsplacedcompanywise',
+  eligibilityControllers.getSelectedStudentsCompanyWise,
+);
+router.get(
+  '/studentsplacedlpawise',
+  eligibilityControllers.getSelectedStudentsLpaWise,
+);
 router.get('/companyWisePackage', eligibilityControllers.getCompanyWisePackage);
 
 export default router;
